b212ba7936b6f871f3af004ea148ba6b4b7facd1
sneka9102000 <96578887+sneka9102000@users.noreply.github.com>
2022-02-25 10:40:45 +0530

    Add files via upload

---
+++ HTML,CSS,JS/login2.js
@@ -0,0 +1,13 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username="admin"&& password=="user")
+    {
+        alert("Login Successfully");
+        return false;
+    }
+    else{
+        alert("Login Failed");
+    }
+}
