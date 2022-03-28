275cad1f770c45dee5ce78813aadf9f1d8b81750
sneka9102000 <96578887+sneka9102000@users.noreply.github.com>
2022-03-28 21:12:53 +0530

    Delete buttonclick.js

--- webstorage API/buttonclick.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.clickcount) {
-    localStorage.clickcount = Number(localStorage.clickcount) + 1;
-  } else {
-    localStorage.clickcount = 1;
-  }
-  document.getElementById("result").innerHTML = "The button is clicked " +
-  localStorage.clickcount + " time(s)."; 
