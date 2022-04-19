b1b2b852b97e1d2c82c2834d6f84a127029197ac
sneka9102000 <96578887+sneka9102000@users.noreply.github.com>
2022-04-19 18:40:10 +0530

    Update index.js

--- EMPLOYEE registration form/index.js
+++ EMPLOYEE registration form/index.js
@@ -20,7 +20,7 @@ function validateBasics() {
   let confirmPasswordVal = confirmPassword();
   let firstNameVal = firstname();
   console.log(emailVal, passwordVal, confirmPasswordVal, firstNameVal);
-  if (emailVal && passwordVal && confirmPasswordVal && firstNameVal == true) {
+  if (emailVal && passwordVal && confirmPasswordVal && firstNameVal) {
     return true;
   }else {
     return false;
@@ -29,8 +29,10 @@ function validateBasics() {
 
 
 function validateEducation() {
-  let yopo = valiYearOfPassout();
-  if (yopo === true) {
+  let year = valiYearOfPassout();
+  let graduation=graduationValidation();
+  // console.log(graduation+"ss")
+  if (year&&graduation === true) {
     return true
   }else {
     return false
@@ -40,7 +42,7 @@ function validateEducation() {
 
 function validateTeam() {
   let teamval = teamValidation();
-  let check = chcekBoxValidation();
+  let check = checkBoxValidation();
   if (teamval && check === true) {
     printThankyou();
     return true;
@@ -50,12 +52,14 @@ function validateTeam() {
 }
 
 
-function chcekBoxValidation() {
+function checkBoxValidation() {
   let tandc = document.getElementById("terms");
   if (tandc.checked === false) {
     console.log("Check the Terms and Conditions")
+    document.getElementById("termsError").innerHTML="Check the Terms and Conditions"
     return (false)
   }else {
+    document.getElementById("termsError").innerHTML=""
     console.log("true");
     return true
   }
@@ -64,11 +68,13 @@ function chcekBoxValidation() {
 
 function teamValidation() {
   let teamName = document.getElementById("practise")
-  if (teamName.options[teamName.selectedIndex].value === "Practise") {
-    console.log("Select a Team")
+  if (teamName.options[teamName.selectedIndex].value === "Practice") {
+    // console.log()
+    document.getElementById("practiseError").innerHTML="Select a Team"
     return false;
 
   }else {
+    document.getElementById("practiseError").innerHTML=""
     console.log("true")
     return true;
   }
@@ -76,15 +82,39 @@ function teamValidation() {
 
 
 function valiYearOfPassout() {
+  const yearOfPassing=/^(((0)[1-9])|((1)[0-2]))(\/)\d{4}$/;
+  // console.log(yearOfPassing.test(document.getElementById("yearofpass").value))
   if (document.getElementById("yearofpass").value === "") {
     console.log("Enter year of passout")
+    document.getElementById("yopError").innerText="Year of Passing Required";
     return false
-  }else {
+  }
+  else if(yearOfPassing.test(document.getElementById("yearofpass").value)==false){
+    
+    document.getElementById("yopError").innerText="Enter in valid format";
+    return false;
+
+  }
+  else {
+    document.getElementById("yopError").textContent=""
     console.log("true")
     return true
   }
 }
 
+// function validateState()
+// {
+//   let formState=document.getElementById("state")
+//   if(formState.options[formState.selectedIndex].value=="Choose a state")
+//   {
+//     console("Select a State");
+//     return false;
+//   }
+//   else{
+//     stateCorrect();
+//     return true;
+//   }
+// }
 
 function validationOthers() {
   let dobVal = validateDOB();
@@ -136,7 +166,8 @@ function validateWorkForm() {
 }
 
 
-function validateEducationForm() {
+function validateEducationForm() 
+{
   if (validateEducation() === true) {
     updateForms();
     return true;
@@ -163,7 +194,7 @@ function validateEmail() {
 
 function validatePassword() {
   console.log("step4")
-  const regexpattern = /^[a-zA-Z]{6,16}$/;
+  const regexpattern =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[#,@,$,%,!]).{8,}$/;
   let password = document.getElementById('password').value
   var result = document.getElementById('password').value.match(regexpattern);
   if (password == "") {
@@ -180,10 +211,11 @@ function validatePassword() {
 
 
 function confirmPassword() {
-  if (document.getElementById("confirmPassword").value == "") {
+  if (document.getElementById("confirmPassword").value === "") {
     document.getElementById('confirmError').textContent = "Password Confirmation Required";
     return false;
   }else if (document.getElementById('confirmPassword').value === document.getElementById('password').value) {
+    document.getElementById('confirmError').textContent = "";
     return true;
   }else {
     document.getElementById('confirmError').textContent = "Please enter the password correctly";
@@ -198,14 +230,15 @@ function firstname() {
   if (!regName.test(name)) {
     document.getElementById('firstError').textContent = "Please enter your first name";
     return false;
-  }else {
+  } else {
+    document.getElementById('firstError').textContent = "";
     return true;
   }
 }
 
 
 function validatePhone() {
-  var phoneno = /^\d{10}$/;
+  var phoneno =/^[6789]{1}[\d]{9}$/;
   var pn = document.getElementById('mobilenumber').value.match(phoneno);
   if (!pn) {
     document.getElementById('mobError').textContent = "Please enter a valid phone number";
@@ -217,14 +250,31 @@ function validatePhone() {
 }
 
 
-function validateYOE() {
-  var vy = document.getElementById('experience')
-  if (vy > 1) {
-    return true;
-  }else {
+function graduationValidation()
+{
+  if(document.getElementById("sslc").checked==false&&document.getElementById("hsc").checked==false&&document.getElementById("ug").checked==false&&document.getElementById("pg").checked==false)
+  {
+    document.getElementById("graduationError").textContent="Select a Graduation";
+    console.log("clicked")
     return false;
   }
+  else{
+    document.getElementById("graduationError").textContent=""
+    return true;
+  }
 }
+
+
+// function validateYOE() {
+//   var vy = document.getElementById('experience')
+//   if (vy > 1) {
+//     return true;
+//   }else {
+//     return false;
+//   }
+// }
+
+
 
 function validateDOB() {
   var dob = document.getElementById('dob').value;
@@ -232,6 +282,7 @@ function validateDOB() {
   var now = new Date();
   var dateDiff = now.getFullYear() - dateFormat.getFullYear();
   if (dateDiff >= 18 && dateDiff <= 60) {
+    document.getElementById('dobError').textContent = "";
     return true;
   }else {
     document.getElementById('dobError').textContent = "Please select the correct dob";
@@ -272,6 +323,7 @@ function updateProgressbar() {
   progress.style.width =
     ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
+
 function printThankyou() {
   let form = document.getElementById("form")
   let final = document.getElementById("final");
